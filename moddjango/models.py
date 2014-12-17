b7b17b631f882b993d732d910367f138b6a79810
anton <anstelli@gmail.com>
2014-12-17 18:55:29 +0300

    0.6.1 very simple frontend with bootstrap for modules; fix bug when first post return last post >_>

--- moddjango/models.py
+++ moddjango/models.py
@@ -14,23 +14,3 @@ class Module(models.Model):
         symmetrical=False,
         blank=True,
         null=True)
-
-info_posts = {
-    "name":"posts",
-    "description":"just added posts list and details for post. with this module u can add ur post in django-admin-panel and look it in ur website",
-    "version":"0.1",
-    "dependencies":[],
-    "migrate":"True",
-    "templates":"True",
-    "urls":"True"
-}
-
-info_comments = {
-    "name":"comments",
-    "description":"added comments for posts. with this module anyone can add comment for ur posts",
-    "version":"0.1.2",
-    "dependencies":["posts"],
-    "migrate":"True",
-    "templates":"True",
-    "urls":"True"
-}
